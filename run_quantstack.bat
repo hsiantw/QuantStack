6baa1e2a4bcb2f149f011c2b18c5ff3a89cbfdbf
hsiantw <hsiantw@gmail.com>
2025-09-06 21:44:49 +0800

    yes

---
+++ run_quantstack.bat
@@ -0,0 +1,4 @@
+@echo off
+cd /d "C:\Users\ian\Desktop\Program\QuantStack\QuantStack-main"
+call venv\Scripts\activate.bat
+python -m streamlit run app.py --server.address 127.0.0.1 --server.port 8501
